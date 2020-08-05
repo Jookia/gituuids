8443bdf61592d6e89380186496e66cfdfc2b3520
aa9ddef0-3f30-4b3e-b9dd-362e497dbc01 <uuid@uuid.uuid>
2020-08-05 19:16:22 +1000

    ok

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@
 git config log.mailmap true
 git config mailmap.blob uuid-map:uuid-map
 ./update-uuids.sh
-./create-my-uuid.sh
+./sync-uuid.sh
+UUID=$(cat /proc/sys/kernel/random/uuid)
+./create-my-uuid.sh $UUID "$(git config user.name)" "$(git config user.email)"
+git config user.name $UUID
+git config user.email uuid@uuid.uuid
